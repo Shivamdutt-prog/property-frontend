ba80d2dc6aa7fd734e2a26a21d79f6c771bfd379
shivam dutt <shivamdutt284@gmail.com>
2026-05-20 19:35:50 +0530

    fix typo in admin url

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -23,7 +23,7 @@ function Admin() {
     const handleLoginSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetch(`http://${IP}:5000/admin-login`, {
+            const response = await fetch(`https://property-backend-of4t.onrender.com/admin-login`, {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(loginData)
